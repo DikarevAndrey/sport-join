1002c637e450613c9e71112d037142a25e210bbc
DikarevAndrey <dias7707@gmail.com>
2017-10-14 01:44:28 +0300

    Добавлены класс District и соответствующие поля в другие классы. Создан view личного кабинета

--- Playgrounds/Classes.swift
+++ Playgrounds/Classes.swift
@@ -20,11 +20,13 @@ class Feedback {
 }
 
 class Playground {
+    var district: District
     var length, width: Int
     var rating, sumOfGrades, quantityOfGrades: Double
     var lighting, covered: Bool
     var cover, kindOfSport: String
-    init (lighting: Bool, covered: Bool, cover: String, length: Int, width: Int, kindOfSport: String) {
+    init (district: District, lighting: Bool, covered: Bool, cover: String, length: Int, width: Int, kindOfSport: String) {
+        self.district = district
         self.rating = 0
         self.quantityOfGrades = 0
         self.sumOfGrades = 0
@@ -41,19 +43,38 @@ class Playground {
         self.quantityOfGrades += 1
     }
 }
-
+class District {
+    var city, name: String
+    var playgrounds = [Playground]()
+    var amountOfPlaygrounds: Int
+    var users = [User]()
+    init(city: String, name: String) {
+        self.city = city
+        self.name = name
+        self.playgrounds = [Playground]()
+        self.amountOfPlaygrounds = 0
+        self.users = [User]()
+    }
+    func addUser(user: User) {
+        users.append(user)
+    }
+    func addPlayground(playground: Playground) {
+        playgrounds.append(playground)
+    }
+}
 class User {
-    
     var name, surname, login, password, phoneNumber: String
     var playgrounds = [Playground]()
     var age: Int
-    init(name: String, surname: String, login: String, password: String, phoneNumber: String, age: Int) {
+    var district: District
+    init(name: String, surname: String, login: String, password: String, phoneNumber: String, age: Int, district: District) {
         self.name = name
         self.surname = surname
         self.login = login
         self.password = password
         self.phoneNumber = phoneNumber
         self.age = age
+        self.district = district
         self.playgrounds = [Playground]()
     }
     func addPlayground(playground: Playground) {
